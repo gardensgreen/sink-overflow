d3611337be539e2793ba895b35ee80bc9240d7bb
Rashmi <monajain99@hotmail.com>
2020-11-04 12:02:14 -0500

    wip

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ const { Answer, Comment, Question, User, Vote, sequelize } = db;
 const { asyncHandler } = require("../utils");
 
 /* GET home page. */
+function convertDate(d){
+ var parts = d.split(" ");
+ var months = {Jan: "01",Feb: "02",Mar: "03",Apr: "04",May: "05",Jun: "06",Jul: "07",Aug: "08",Sep: "09",Oct: "10",Nov: "11",Dec: "12"};
+ return parts[3]+"/"+months[parts[1]]+"/"+parts[2];
+}
+
 router.get(
     "/",
     asyncHandler(async (req, res) => {
